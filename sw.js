d1e5840eb1ce726a073a84abcd489cc559e3c62c
riku-lab1121 <ricu11ha@gmail.com>
2026-08-19 10:24:17 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,17 @@
-const CACHE='vending-route-v6-5';
+const CACHE='vending-route-v7-0';
 const CORE=['./','./index.html','./manifest.json'];
-self.addEventListener('install',event=>{
+self.addEventListener('install',e=>{
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));
 });
-self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET')return;
-  event.respondWith(
-    fetch(event.request)
-      .then(response=>{
-        const copy=response.clone();
-        caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
-        return response;
-      })
-      .catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html')))
-  );
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(fetch(e.request).then(r=>{
+    const copy=r.clone();
+    caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
+    return r;
+  }).catch(()=>caches.match(e.request).then(hit=>hit||caches.match('./index.html'))));
 });
