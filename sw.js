969b7ffbd194d1c65deac69d217df196a0938447
riku-lab1121 <ricu11ha@gmail.com>
2026-08-21 13:40:10 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vending-route-v11-10-0-map-detail-page';
+const CACHE='vending-route-v11-11-0-planning-sync';
 const CORE=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
